ad1459af82491f904aac3057864343d9594288da
ezalden0 <ezalden.abd@gmail.com>
2022-11-10 17:01:00 +0200

    first commit

---
+++ js/app.js
@@ -0,0 +1,8 @@
+let spinner = document.querySelector('.spinner');
+let spin = document.querySelector('.spinner-border text-secondary');
+let span = document.querySelector('.sr-only');
+
+window.onload = function(){
+    spinner.classList.add('hide');
+    spinner.remove();
+}
